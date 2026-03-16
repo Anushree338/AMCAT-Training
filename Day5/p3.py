38279292068070270776346ac6e72a2109377479
Anushree Bonde <anushreebonde338@gmail.com>
2026-03-16 19:28:54 +0530

    Add files via upload

---
+++ Day5/p3.py
@@ -0,0 +1,10 @@
+#positional argument
+#keyword argument
+#default argument
+def personalInfo(fname, lname):
+    print("first name:", fname)
+    print("last name:", lname)
+
+fname = "anushree"
+lname = "bonde"
+personalInfo(fname,lname)
